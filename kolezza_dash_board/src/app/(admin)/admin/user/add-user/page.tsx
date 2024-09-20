3baa5469b209b31ca324715b83040a91a5a6d004
Mary-Njiru <marynwanjiruu@gmail.com>
2024-09-20 18:49:24 +0300

    adds install in workflow

---
+++ kolezza_dash_board/src/app/(admin)/admin/user/add-user/page.tsx
@@ -0,0 +1,17 @@
+// import UserRegistration from "@/app/components/AddUser";
+// import Layout from "@/app/Layout";
+import UserRegistration from "@/app/components/AddUser";
+import Layout from "@/app/Layout";
+
+const AddUser = () => {
+    return(
+        <Layout>
+            <UserRegistration/>
+        </Layout>
+    )
+}
+
+export default AddUser;
+
+
+
